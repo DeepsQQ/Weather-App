cff9592a1ffd4977b21d6f251e957648ad07ed24
DeepsQQ <unixpars@gmail.com>
2023-05-08 00:16:11 +0500

    feat(current weather): add root element

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import CurrentWeather from "./components/CurrentWeather";
 
 const App = () => {
   return (
     <>
       <Header />
+      <CurrentWeather />
     </>
   );
 };
---
+++ src/components/CurrentWeather/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./currentWeather.module.scss";
+
+const CurrentWeather = () => {
+  return (
+    <main className={styles.root}>
+      <div className={styles.container}></div>
+    </main>
+  );
+};
+
+export default CurrentWeather;
