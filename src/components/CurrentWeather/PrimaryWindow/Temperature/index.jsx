d7a527679fa38bef9d4d560b94f6c13900d4ef30
DeepsQQ <unixpars@gmail.com>
2023-05-21 15:02:14 +0500

    feat(CurrentWeather): add temperature in PrimaryWindow

---
+++ src/components/CurrentWeather/PrimaryWindow/Temperature/index.jsx
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux";
+
+import styles from "./temperature.module.scss";
+
+const icons = require.context("../../../../icons");
+
+const Temperature = () => {
+  const { temp_c, temp_f, feelslike_c, feelslike_f, is_day, condition } =
+    useSelector((state) => state.weatherData.currentWeather);
+  const degreesMode = useSelector((state) => state.degreesMode.mode);
+
+  const iconPath = `./${is_day ? "day" : "night"}/${condition.code}.svg`;
+  const degrees = degreesMode === "C" ? temp_c : temp_f;
+  const feelsLike = degreesMode === "C" ? feelslike_c : feelslike_f;
+
+  return (
+    <div>
+      <div className={styles.temperature}>
+        <div className={styles.degrees}>{Math.trunc(degrees)}</div>
+        <div className={styles.degreesMode}>{`°${degreesMode}`}</div>
+        <img
+          className={styles.weatherIcon}
+          src={icons(iconPath)}
+          alt={condition.text}
+        />
+      </div>
+      <div className={styles.feelsLike}>{`Feels like ${Math.trunc(
+        feelsLike
+      )}°`}</div>
+      <div className={styles.weatherText}>{condition.text}</div>
+    </div>
+  );
+};
+
+export default Temperature;
--- src/components/CurrentWeather/PrimaryWindow/index.jsx
+++ src/components/CurrentWeather/PrimaryWindow/index.jsx
@@ -1,11 +1,15 @@
 import styles from "./primaryWindow.module.scss";
 
 import TimeAndGeolocation from "./TimeAndGeolocation";
+import Temperature from "./Temperature";
 
 const PrimaryWindow = () => {
   return (
     <div className={styles.root}>
       <TimeAndGeolocation />
+      <div className={styles.weatherInfo}>
+        <Temperature />
+      </div>
     </div>
   );
 };
