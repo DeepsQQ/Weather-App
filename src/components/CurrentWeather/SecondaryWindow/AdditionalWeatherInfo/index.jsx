6b8a9c61b87db228b560df82bd5b2b8a7169e606
DeepsQQ <unixpars@gmail.com>
2023-06-02 17:34:05 +0500

    feat(CurrentWeather): add additional weather info in second window

---
+++ src/components/CurrentWeather/SecondaryWindow/AdditionalWeatherInfo/index.jsx
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux";
+
+import styles from "./additionalWeatherInfo.module.scss";
+
+const AdditionalWeatherInfo = () => {
+  const { vis_km, uv } = useSelector(
+    (state) => state.weatherData.currentWeather
+  );
+  const { moon_phase, moon_illumination } = useSelector(
+    (state) => state.weatherData.forecast[0].astro
+  );
+
+  return (
+    <ul className={styles.root}>
+      <li>
+        <span className={styles.title}>UV-Index:&nbsp;</span>
+        <span>{uv}</span>
+      </li>
+      <li>
+        <span className={styles.title}>Visibility:&nbsp;</span>
+        <span>{vis_km + " km"}</span>
+      </li>
+      <li>
+        <span className={styles.title}>Moon Illumination:&nbsp;</span>
+        <span>{moon_illumination + "%"}</span>
+      </li>
+      <li>
+        <span className={styles.title}>Moon Phase:&nbsp;</span>
+        <span>{moon_phase}</span>
+      </li>
+    </ul>
+  );
+};
+
+export default AdditionalWeatherInfo;
--- src/components/CurrentWeather/SecondaryWindow/index.jsx
+++ src/components/CurrentWeather/SecondaryWindow/index.jsx
@@ -1,11 +1,13 @@
 import styles from "./secondaryWindow.module.scss";
 
 import DayProgress from "./DayProgress";
+import AdditionalWeatherInfo from "./AdditionalWeatherInfo";
 
 function SecondaryWindow() {
   return (
     <div className={styles.root}>
       <DayProgress />
+      <AdditionalWeatherInfo />
     </div>
   );
 }
