bd79070de768b7b07639a300e3664fc31fc4badd
K1LORIN <tima228rus@gmail.com>
2023-06-13 00:04:12 +0500

    feat(Current): add skeleton

--- src/components/Forecast/index.jsx
+++ src/components/Forecast/index.jsx
@@ -1,9 +1,27 @@
+import Skeleton from "react-loading-skeleton";
+import { useSelector } from "react-redux";
+
 import styles from "./forecast.module.scss";
 
+const ForecastSkeleton = ({ count }) => {
+  const skeletons = Array.from(Array(count), (value, index) => (
+    <div key={index} className={styles.skeleton}>
+      <Skeleton height={140} />
+      <Skeleton height={140} />
+    </div>
+  ));
+
+  return skeletons;
+};
+
 const Forecast = () => {
+  const forecastData = useSelector((state) => state.weatherData.forecast);
+
   return (
     <div className={styles.root}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        {forecastData ? true : <ForecastSkeleton count={3} />}
+      </div>
     </div>
   );
 };
