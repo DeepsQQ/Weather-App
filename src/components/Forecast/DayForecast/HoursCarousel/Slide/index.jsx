73f38ae9725f4c6d5b8737ed21e1afee663759df
DeepsQQ <unixpars@gmail.com>
2023-07-01 20:57:25 +0500

    feat(Forecast): round up numbers in hours carousel

--- src/components/Forecast/DayForecast/HoursCarousel/Slide/index.jsx
+++ src/components/Forecast/DayForecast/HoursCarousel/Slide/index.jsx
@@ -8,13 +8,15 @@ const Slide = ({ time, temperature, condition, isDay }) => {
   const degreesMode = useSelector((state) => state.degreesMode.mode);
   const htmlTime = time.split(" ")[1];
   const iconPath = `./${isDay ? "day" : "night"}/${condition.code}.svg`;
+  const сelsius = Math.round(temperature.c);
+  const fahrenheit = Math.round(temperature.f);
 
   return (
     <div className={styles.root}>
       <div>{htmlTime}</div>
       <img className={styles.icon} src={icons(iconPath)} alt={condition.text} />
       <div className={styles.temperature}>
-        {degreesMode === "C" ? temperature.c : temperature.f}°
+        {degreesMode === "C" ? сelsius : fahrenheit}°
       </div>
     </div>
   );
