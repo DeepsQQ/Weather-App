e8b857abd5a02e312f509862e2ffd8140e379ffe
DeepsQQ <unixpars@gmail.com>
2023-07-03 00:14:31 +0500

    feat(Footer): change html tag to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,12 +2,12 @@ import styles from "./footer.module.scss";
 
 const Footer = () => {
   return (
-    <div className={styles.root}>
+    <footer className={styles.root}>
       Powered by&nbsp;
       <a className={styles.link} href="https://www.weatherapi.com/">
         WeatherAPI.com
       </a>
-    </div>
+    </footer>
   );
 };
 
